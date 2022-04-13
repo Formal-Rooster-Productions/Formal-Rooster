4932ceeec9b4b3c5bb72ab0ea689b78c2ecfb28a
Evan Amundson <amundson.9001@gmail.com>
2022-04-13 18:30:40 -0500

    finished player movements

--- Assets/scripts/HeroModel.cs
+++ Assets/scripts/HeroModel.cs
@@ -9,34 +9,79 @@ namespace Assets
 {
     public class HeroModel : MonoBehaviour
     {
-        private Vector2 playerVelocity = new Vector2(2f,10f);
-        private Animator _animator;
-        private SpriteRenderer _renderer;
+        public float jumpPower = 5f;
+        public float moveSpeed = 3f;
+        private bool jump = false;
+        private Animator _animator; //References players animator
+        private SpriteRenderer _renderer; //References players Renderer Component
+        private Rigidbody2D _rb; //References Players Rigidbody component
+        
+
+        public LayerMask groundLayer;
 
         public void Start()
         {
             _animator = GetComponent<Animator>();
             _renderer = GetComponent<SpriteRenderer>();
+            _rb = GetComponent<Rigidbody2D>();
             _animator.Play("Chicken Idle");
             _animator.enabled = true;
         }
 
         public void Update()
         {
-            float inputX = Input.GetAxis("Horizontal");
-            float inputY = Input.GetAxis("Vertical");
+            Debug.DrawRay(transform.position, Vector2.down, Color.green);
 
-            Vector3 movement = new Vector3(playerVelocity.x * inputX, playerVelocity.y * inputY, 0);
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded())
+            {
+                jump = true;
+            }
 
-            if (movement.x != 0f) { _animator.Play("Chicken Run"); }
+            var direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+            if (direction.x != 0f) { _animator.Play("Chicken Run"); }
             else { _animator.Play("Chicken Idle"); }
-            _renderer.flipX = movement.x > 0f;
 
-            movement *= Time.deltaTime;
+            if (direction.x != 0)
+            {
+                _renderer.flipX = direction.x > 0f;
+            }
 
-            transform.Translate(movement);
+           
 
-  
+        }
+
+        public void FixedUpdate()
+        {
+            if (jump)
+            {
+                Jump();
+               
+            }
+        }
+
+        void Jump()
+        {
+            //_playerVelocity.y = jumpPower;
+            _rb.velocity = Vector2.up * jumpPower;
+            jump = false;
+        }
+
+        bool isGrounded()
+        {
+            Vector2 position = transform.position;
+            Vector2 direction = Vector2.down;
+            float distance = 1.0f;
+
+            RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+            
+            if (hit.collider != null)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
